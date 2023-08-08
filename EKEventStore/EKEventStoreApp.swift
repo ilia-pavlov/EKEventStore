6071ed7fc257a7e868474bbc66dee6d521f3c220
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-08 16:45:07 -0400

    Initial Commit

---
+++ EKEventStore/EKEventStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EKEventStoreApp.swift
+//  EKEventStore
+//
+//  Created by Ilia Pavlov on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EKEventStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
